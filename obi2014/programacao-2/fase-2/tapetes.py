b996ced49a3a9edebcd69107345a3f4318f16a59
felipemfp <felipemfpontes@gmail.com>
2016-04-11 14:18:55 -0300

    Finaliza prova OBI2014 Programação 2 - Fase 2

--- obi2014/programacao-2/fase-2/tapetes.py
+++ obi2014/programacao-2/fase-2/tapetes.py
@@ -1,1 +1,9 @@
-# status: nada aqui
+# status: testado com exemplos da prova
+
+if __name__ == '__main__':
+    length, count = [int(x) for x in input().strip().split(' ')]
+
+    rug_1 = count - 1
+    rug_max = length - rug_1
+
+    print(rug_max**2 + rug_1)
