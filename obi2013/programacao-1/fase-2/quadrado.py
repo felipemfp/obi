b07fcbd0f4170e0b8ee52326e0ca0b5e9d8f9637
Felipe Pontes <felipemfpontes@gmail.com>
2016-04-13 23:43:06 -0300

    Resolve problema do Quadradinho de 8 (OBI2013)

---
+++ obi2013/programacao-1/fase-2/quadrado.py
@@ -0,0 +1,20 @@
+# status: testado com exemplos da prova
+
+if __name__ == '__main__':
+    n = int(input())
+    cubes = [int(x) for x in input().split(' ')]
+
+    ways = 0
+
+    for x in range(0, n):
+        x_sum = cubes[x]
+        for y in range(x, n):
+            if x == y:
+                if cubes[y] % 8 == 0:
+                    ways += 1
+            else:
+                x_sum += cubes[y]
+                if x_sum % 8 == 0:
+                    ways += 1
+
+    print(ways)
