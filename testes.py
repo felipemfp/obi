b83ee87d16e89f58933c402b2cc8c9719ac7e838
Francisco Bento <francisco.bento.jr@hotmail.com>
2016-07-30 12:38:00 -0300

    Atualiza script de testes

--- testes.py
+++ testes.py
@@ -1,57 +1,74 @@
 from os import listdir, path
-import sys, subprocess
+import sys
+import subprocess
 
+
+class Colors:
+    HEADER = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 def get_tests_results(question):
     main_folder = question
     script = "{0}.py".format(question)
 
     tests = [test for test in listdir(main_folder)]
+    right_answers = 0
+    total_answers = 0
 
+    # sys.stdout.write('{}.py\n\t'.format(question.split('/')[-1]))
     for test_folder in tests:
 
         test_folder_name = "{0}/{1}".format(main_folder, test_folder)
         tests_cases = [case for case in listdir(test_folder_name)]
-        tests_results = []
-
         for index in range(int(len(tests_cases) / 2)):
-            case_input_name = "{0}/{1}{2}".format(test_folder_name, "in",index + 1)
-            case_output_name = "{0}/{1}{2}".format(test_folder_name, "out",index + 1)
+            case_input_name = "{0}/{1}{2}".format(
+                test_folder_name, "in", index + 1)
+            case_output_name = "{0}/{1}{2}".format(
+                test_folder_name, "out", index + 1)
             input_file = open(case_input_name)
-            proc = subprocess.Popen(['python', script], stdin=input_file, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-            result = proc.communicate()[0].decode('utf-8').strip()
+            proc = subprocess.Popen(
+                ["python", script], stdin=input_file, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+            result = proc.communicate()[0].decode("utf-8").strip()
             output_file = open(case_output_name).read().strip()
-            tests_results.append('.' if result == output_file else 'F')
-            print('.' if result == output_file else 'F', end='')
-            print(end='')
 
+            if result == output_file:
+                right_answers += 1
+                sys.stdout.write('.')
+            else:
+                sys.stdout.write('F')
+            total_answers += 1
+            sys.stdout.flush()
 
-        # print('Teste', "%2s" % test_folder, ''.join(tests_results))
+    return right_answers, total_answers
 
 if __name__ == "__main__":
-    obis = [f for f in listdir('.') if path.isdir(f) and ("obi" in f)]
+    obis = [f for f in listdir(".") if path.isdir(f) and ("obi" in f)]
 
     for obi in obis:
+        print(obi.upper())
         levels = [f for f in listdir(obi)]
-        print(levels)
         for level in levels:
+            print(" {0}".format(level))
             level = "{0}/{1}".format(obi, level)
             stages = [f for f in listdir(level)]
             for stage in stages:
+                print("  {0}".format(stage))
                 stage = "{0}/{1}".format(level, stage)
                 questions = [f for f in listdir(stage) if f.endswith("py")]
                 for question in questions:
                     name = question
+                    print("   {0} ".format(name), end="")
                     question = "{0}/{1}".format(stage, question)
                     folder = "{0}/{1}".format(stage, path.splitext(name)[0])
-
                     if path.isdir(folder):
-                        # print("Testando questão", name, end='\n')
-                        get_tests_results(folder)
+                        right_answers, total_answers = get_tests_results(folder)
+                        color = Colors.GREEN if right_answers == total_answers else Colors.FAIL
+                        print("   {1}/{2}".format(name,right_answers, total_answers), end='')
 
-                print(questions)
-            print(stages)
-
-    print('\n'.join(obis))
-
-    # get_tests_results("obi2012/programacao-1/fase-1/vice")
+                    print("")
